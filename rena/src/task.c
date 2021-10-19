ec135ba45724ee98a0c7588f2973077363de000a
Ricardo Rodrigues Lucca <ricardo.lucca@rnp.br>
2021-10-19 13:40:17 -0300

    adicionando pthread

--- rena/src/task.c
+++ rena/src/task.c
@@ -2,19 +2,64 @@
 #include "global.h"
 #include "task_manager.h"
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+
+typedef struct task {
+    int idle;
+    int accepting;
+    time_t last_change;
+    pthread_t tid;
+} task_t;
+
+static void *dummy(void *arg) {
+struct rena *rena = (struct rena *) arg;
+for (;rena->exit == 0;) pause(); // trava na espera de sinal...
+return NULL; }
+
 struct task *task_create(int number,
                          struct rena *rena)
 {
-    return NULL;
+    struct task *ret = calloc(1, sizeof(struct task));
+    if (number == 0)
+    {
+        ret->tid = pthread_self();
+    } else {
+        if (pthread_create(&ret->tid, NULL,
+                           &dummy, rena) != 0)
+        {
+            do_log(LOG_ERROR,
+                   "pthread_create returned -1 (%m)");
+            free(ret);
+            return NULL;
+        }
+    }
+
+    ret->idle = 1;
+    ret->accepting = 1;
+    ret->last_change = time(NULL);
+    return ret;
 }
 
 void task_release(int number, struct task **task)
 {
-    (void) number;
-    (void) task;
+    if (task == NULL || *task == NULL)
+    {
+        return ;
+    }
+
+    if (number != 0)
+    {
+        pthread_join((*task)->tid, NULL);
+    }
+
+    free(*task);
+    *task = NULL;
 }
 
 void task_mainthread_work(struct rena *rena)
 {
-    (void) rena;
+    dummy(rena);
 }
