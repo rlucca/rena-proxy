e3c6b43c8da5dcdf63d785bb5e5e6c960df508d1
Ricardo Rodrigues Lucca <ricardo.lucca@rnp.br>
2022-04-22 15:19:30 -0300

    logger: mecanismo de log com stderr e syslog
    Agora temos dois sistemas de logs um usando fprintf e outro usando o syslog.
    O arquivo de testes teve que ser mexido porque por padrao sera o stderr e
    quando mencionado o syslog. Alem disso, mesmo o syslog tendo um filtro iremos
    realizar o filtro de nivel de log antes de realizar a chamada da funcao.

--- rena/src/logger.c
+++ rena/src/logger.c
@@ -1,17 +1,27 @@
 #include "global.h"
 #include <stdarg.h>
 #include <syslog.h>
+#include <string.h>
 #include <pthread.h>
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
+static void syslog_reset(int level, int options,
+                         int facility, const char *ident);
+static void syslog_message(int level, const char *format, va_list *ptr);
+static void stderr_message(int level, const char *format, va_list *ptr);
+
+static struct log_handler_t {
+        void (*reset)(int, int, int, const char *);
+        void (*message)(int, const char *, va_list *);
+        int minimum;
+    } log_handler_fnc = { NULL, stderr_message, LOG_DEBUG };
 
 
 void logger_reset(int level, int options, int facility,
                   const char *ident)
 {
-    closelog();
-    openlog(ident, options, facility);
-    setlogmask(LOG_UPTO(level));
+    if (log_handler_fnc.reset != NULL)
+        log_handler_fnc.reset(level, options, facility, ident);
 
     logger_message(LOG_DEBUG, "logger reseted");
 }
@@ -21,7 +31,10 @@ void logger_message(int level, const char *format, ...)
     va_list ptr;
     pthread_mutex_lock(&lock);
     va_start(ptr, format);
-    vsyslog(level, format, ptr);
+
+    if (log_handler_fnc.message != NULL && level <= log_handler_fnc.minimum)
+        log_handler_fnc.message(level, format, &ptr);
+
     va_end(ptr);
     pthread_mutex_unlock(&lock);
 }
@@ -29,6 +42,7 @@ void logger_message(int level, const char *format, ...)
 void logger_reconfigure(void *arg)
 {
     struct config_rena *c = (struct config_rena *) arg;
+    const char *engine = NULL;
     int logging[3];
 
     if (c == NULL)
@@ -38,11 +52,41 @@ void logger_reconfigure(void *arg)
     }
 
     pthread_mutex_lock(&lock);
+    config_get_logging_engine(&c, &engine);
     config_get_logging_minimum(&c, &logging[0]);
     config_get_logging_options(&c, &logging[1]);
     config_get_logging_facility(&c, &logging[2]);
+    log_handler_fnc.minimum = logging[0];
+    if (strcmp("syslog", engine) == 0)
+    {
+        log_handler_fnc.reset = syslog_reset;
+        log_handler_fnc.message = syslog_message;
+    } else {
+        log_handler_fnc.reset = NULL;
+        log_handler_fnc.message = stderr_message;
+    }
     pthread_mutex_unlock(&lock);
 
     logger_reset(logging[0], logging[1], logging[2], NULL);
     do_log(LOG_DEBUG, "done");
+}
+
+static void syslog_reset(int level, int options,
+                         int facility, const char *ident)
+{
+    closelog();
+    openlog(ident, options, facility);
+    setlogmask(LOG_UPTO(level));
+}
+
+static void syslog_message(int level, const char *format, va_list *ptr)
+{
+    vsyslog(level, format, *ptr);
+}
+
+static void stderr_message(int level, const char *format, va_list *ptr)
+{
+    fprintf(stderr, "LVL %d - ", level);
+    vfprintf(stderr, format, *ptr);
+    fprintf(stderr, "\n");
 }
--- rena/tests/logger_unit_test.c
+++ rena/tests/logger_unit_test.c
@@ -3,6 +3,7 @@
 //#define CHEAT_TIME 500
 #include "cheat.h"
 
+#include "global.h"
 #include "logger.h"
 
 CHEAT_DECLARE(
@@ -29,7 +30,40 @@ CHEAT_DECLARE(
             setlogmask_called++;
             return 0;
         }
+
+        struct config_rena // copied from config.c
+        {
+            char certificate_file[MAX_FILENAME];
+            char certificate_key[MAX_FILENAME];
+            char database_directory[MAX_FILENAME];
+            char database_suffix[MAX_STR];
+            char server_bind[MAX_STR];
+            int server_port_http;
+            int server_port_https;
+            int logging_facility;
+            int logging_options;
+            int logging_minimum;
+            int pool_minimum;
+            int pool_maximum;
+            int pool_reap_time;
+            float pool_addictive;
+            char parser_analyze_mime[MAX_STR];
+            char parser_ignore_mime[MAX_STR];
+            char parser_analyze_accept[MAX_STR];
+            char parser_ignore_accept[MAX_STR];
+            char logging_engine[MAX_STR];
+        };
 )
+
+CHEAT_SET_UP(
+        struct config_rena obj;
+        const char *s = "syslog";
+        memcpy(obj.logging_engine, s, strlen(s) + 1);
+        logger_reconfigure(&obj);
+        openlog_called = closelog_called = 0;
+        vsyslog_called = setlogmask_called = 0;
+        setlogmask_mask = 0;
+    )
 
 CHEAT_TEST(call_logger_reset,
     logger_reset(3, 0, 0, NULL);
