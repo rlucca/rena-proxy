d0f1bcf6028372e2c4f2e80d9c3b7a858c0293d2
Ricardo Rodrigues Lucca <ricardo.lucca@rnp.br>
2022-06-08 15:23:19 -0300

    formatter: implementacao das funcoes basicas e testes unitarios
    As funcoes mais basicas foram implementadas juntamente de alguns
    testes unitarios para elas. Foi feito uma maquina de estados para
    avaliar e criar a cadeia de callbacks que serao chamadas no futuro.
    No desenvolvimento foi visto que so precisamos ter conhecimento do
    estado literal e quando retornou para o estado literal nada mais que
    isso. O desenvolvimento da maquina de estados permitiu diminuir a
    complexidade da funcao que existiria no lugar para bem a baixo dos 15.

--- rena/src/formatter.c
+++ rena/src/formatter.c
@@ -3,17 +3,37 @@
 #include "clients.h"
 #include "formatter.h"
 
-typedef int (*handler_t)(char *, size_t *,
-                         struct formatter *, struct formatter_userdata);
+#include <stdlib.h>
+
+struct chain_formatter;
+
+enum {
+    DETECTED_LITERAL = 0,
+    DETECTED_EXPRESSION_AND_MODIFIER,
+    DETECTED_MODIFIER_ONLY,
+    EXPRESSION_START,
+    STATE_INVALID
+};
+
+typedef int (*handler_t)(struct chain_formatter *, struct formatter *,
+                         struct formatter_userdata *);
+
+struct chain_formatter
+{
+    size_t position_start;
+    size_t position_end;
+    int registered_index;
+
+    struct chain_formatter *next;
+};
 
 struct formatter
 {
-    const char *config;
-    size_t position_start;
-    size_t position_end;
-    handler_t fnc;
+    const char *format;
+    size_t format_len;
 
-    struct formatter *next;
+    struct chain_formatter *first;
+    struct chain_formatter *last;
 };
 
 struct modifier_format
@@ -27,7 +47,7 @@ struct modifier_format
 static char modifier_start = '%';
 static char expression_pair[] = "{}";
 static struct modifier_format registered[] = {
-        {  0, '%', 0, NULL }, // copy literal...
+        {  0, '%', 0, NULL }, // literal '%'
         {  1, 'h', 0, NULL },
         {  2, 'l', 0, NULL },
         {  3, 'u', 0, NULL },
@@ -44,7 +64,7 @@ static struct modifier_format registered[] = {
         { 99, 'Y', 0, NULL }, // year
         { 99, 'z', 0, NULL }, // timezone
         { 99, 'Z', 0, NULL }, // timezone
-        { -1,   0, 0, NULL }
+        { -1,   0, 0, NULL }  // copy literal and should be the last one!
     };
 
 
@@ -60,57 +80,172 @@ static int find_modifier(char ch)
     return -1;
 }
 
+static int evaluate_char(char c, int previous_state, int *ret_modifier)
+{
+    int ret = previous_state;
+    // default modifier is that marked as -1 group, zero char, no expression
+    int modifier = sizeof(registered) / sizeof(*registered) - 1;
+    switch (previous_state)
+    {
+    case EXPRESSION_START:
+        if (expression_pair[1] == c)
+        {
+            ret = DETECTED_MODIFIER_ONLY;
+        } else {
+            if (expression_pair[0] == c || modifier_start == c)
+                ret = STATE_INVALID;
+        }
+        break;
+    case DETECTED_EXPRESSION_AND_MODIFIER:
+        if (expression_pair[0] == c)
+        {
+            ret = EXPRESSION_START;
+            break;
+        }
+
+        /* falldown here to test modifier! */
+    case DETECTED_MODIFIER_ONLY:
+        modifier = find_modifier(c);
+        if (modifier < 0)
+            ret = STATE_INVALID;
+        else
+            ret = DETECTED_LITERAL;
+        break;
+
+    default: // START
+        if (modifier_start == c)
+            ret = DETECTED_EXPRESSION_AND_MODIFIER;
+        break;
+    }
+
+    if (ret_modifier)
+        *ret_modifier = modifier;
+
+    return ret;
+}
+
 static int is_a_valid_input(const char *s, size_t s_len)
 {
-    enum {
-        STATE_START = 0,
-        DETECTED_EXPRESSION_AND_MODIFIER,
-        DETECTED_MODIFIER_ONLY,
-        EXPRESSION_START,
-    };
     int ret = 0; // success
-    int state = STATE_START;
+    int state = DETECTED_LITERAL;
     for (size_t m = 0; !ret && m < s_len; m++)
     {
         char c = s[m];
-
-        switch (state)
-        {
-        case EXPRESSION_START:
-            if (expression_pair[1] == c)
-            {
-                state = DETECTED_MODIFIER_ONLY;
-            } else {
-                if (expression_pair[0] == c || modifier_start == c)
-                    ret = -1;
-            }
-            break;
-        case DETECTED_EXPRESSION_AND_MODIFIER:
-            if (expression_pair[0] == c)
-            {
-                state = EXPRESSION_START;
-                continue;
-            }
-
-            /* falldown here to test modifier! */
-        case DETECTED_MODIFIER_ONLY:
-            if (find_modifier(c) < 0)
-                ret = -1;
-            else
-                state = STATE_START;
-            break;
-
-        default: // START or INVALID?
-            if (modifier_start == c)
-                state = DETECTED_EXPRESSION_AND_MODIFIER;
-            break;
-        }
+        int new_state = evaluate_char(c, state, NULL);
+        if (new_state != STATE_INVALID)
+            state = new_state;
+        else
+            ret = -1;
     }
 
-    if (state != STATE_START)
+    if (state != DETECTED_LITERAL)
         ret = -1;
 
     return ret;
+}
+
+static void list_add_at_end(struct formatter *inout,
+                            size_t begin, size_t end, int index)
+{
+    struct chain_formatter *a;
+    a = malloc(sizeof(*a));
+    a->position_start = begin;
+    a->position_end = end;
+    a->registered_index = index;
+    a->next = NULL;
+
+    if (inout->first == NULL)
+    {
+        inout->first = inout->last = a;
+        return ;
+    }
+
+    inout->last->next = a;
+    inout->last = a;
+}
+
+static int list_foreach(struct formatter *inout,
+                        struct formatter_userdata *userdata,
+                        handler_t fnc)
+{
+    do_log(LOG_DEBUG, "TODO");
+    return -1;
+}
+
+static struct formatter *create_list_head(const char *format,
+                                          size_t format_len)
+{
+    struct formatter *ret = NULL;
+    ret = malloc(sizeof(*ret));
+    ret->format = format;
+    ret->format_len = format_len;
+    ret->first = ret->last = NULL;
+    return ret;
+}
+
+static int create_chain_from_format(struct formatter *inout)
+{
+    int ret = -1;
+
+    if (inout)
+    {
+        int state = DETECTED_LITERAL;
+        size_t begin = 0;
+        ret = 0;
+        for (size_t pos = 0; !ret && pos < inout->format_len; pos++)
+        {
+            int index = -1;
+            int new_state = evaluate_char(inout->format[pos], state, &index);
+            if (new_state != STATE_INVALID)
+            {
+                // we only are interested on create an element of chain
+                // when we go to literal state or get out of it
+                if (state != new_state && (DETECTED_LITERAL == new_state
+                    || DETECTED_EXPRESSION_AND_MODIFIER == new_state))
+                {
+                    if (pos == 0)
+                        list_add_at_end(inout, begin, pos, index);
+                    else
+                        list_add_at_end(inout, begin, pos - 1, index);
+
+                    begin = pos;
+                }
+
+                state = new_state;
+            } else {
+                ret = -1;
+            }
+        }
+
+        if (state == DETECTED_LITERAL)
+        {
+        } else {
+            ret = -1;
+        }
+
+        // TODO if (ret < 0) destroy everything!
+    }
+    // alem disso, se ouver agrupadores iguais intercalados por agrupador zero
+    // eles devem ser considerados como somente um unico elemento da cadeia!
+    return ret;
+}
+
+static int parse_input(struct formatter **inout,
+                       const char *format, size_t format_len)
+{
+    if (is_a_valid_input(format, format_len))
+        return -1;
+
+    if (inout != NULL)
+    {
+        *inout = create_list_head(format, format_len);
+        if (create_chain_from_format(*inout))
+        {
+            formatter_destroy_handler(inout);
+        }
+    }
+
+    return 0;
 }
 
 int formatter_create_handler(struct formatter **inout,
@@ -124,11 +259,63 @@ int formatter_create_handler(struct formatter **inout,
         return -1;
     }
 
-    if (is_a_valid_input(format, format_len))
+    if (parse_input(inout, format, format_len))
     {
         do_log(LOG_DEBUG, "problem parsing log format: %.*s",
                (int) format_len, format);
         return -2;
+    }
+
+    return 0;
+}
+
+static int foreach_evaluate(struct chain_formatter *chain,
+                            struct formatter *inout,
+                            struct formatter_userdata *userdata)
+{
+    if (chain->registered_index >= 0)
+    {
+        handler_t fnc = registered[chain->registered_index].fnc;
+        if (fnc)
+            return fnc(chain, inout, userdata);
+    }
+
+    return -1;
+}
+
+static int foreach_destroy(struct chain_formatter *chain,
+                           struct formatter *inout,
+                           struct formatter_userdata *userdata)
+{
+    (void) inout;
+    (void) userdata;
+    free(chain);
+    return 0;
+}
+
+int formatter_destroy_handler(struct formatter **inout)
+{
+    if (inout)
+    {
+        if (list_foreach(*inout, NULL, foreach_destroy))
+        {
+            do_log(LOG_ERROR, "it was not possible to free all data");
+        }
+
+        free(*inout);
+        *inout = NULL;
+    }
+
+    return 0;
+}
+
+int formatter_evaluate(struct formatter **inout,
+                       struct formatter_userdata *userdata)
+{
+    if (inout && list_foreach(*inout, userdata, foreach_evaluate))
+    {
+        do_log(LOG_ERROR, "it was not possible to evaluate all chain");
+        return -1;
     }
 
     return 0;
--- rena/tests/formatter_unit_test.c
+++ rena/tests/formatter_unit_test.c
@@ -9,8 +9,10 @@
 #endif
 
 CHEAT_DECLARE(
-    #define valid_case(P, N, E) \
-        cheat_assert_int32(formatter_create_handler(NULL, P, N), E)
+    #define valid_case(Q, P, N, E) \
+        cheat_assert_int32(formatter_create_handler(Q, P, N), E)
+    struct formatter *trash_ptr = (struct formatter *) 0x10;
+    struct formatter *okay_ptr = NULL;
 )
 
 CHEAT_SET_UP(
@@ -29,25 +31,63 @@ CHEAT_TEST(find_modifier_should_return_position_of_element,
 
     int last = sizeof(registered) / sizeof(*registered) - 2;
     cheat_assert_int32(find_modifier(registered[last].modifier), last);
-    cheat_assert(registered[last + 1].fnc == NULL);
+    cheat_assert_int32(registered[last + 1].group_id, -1);
 )
 
 CHEAT_TEST(is_a_valid_input_test,
-    struct formatter *trash_pointer = (struct formatter *) 0x10;
-    cheat_assert_int32(formatter_create_handler(&trash_pointer, "%b", 2), -1);
+    valid_case(NULL, "%b",  2, 0);
+    valid_case(NULL, "- -", 3, 0);
+    valid_case(NULL, "%{aaa}i %{bb}i", 14, 0);
+    valid_case(NULL, "%",  1, -2);
+    valid_case(NULL, "% ", 2, -2);
+    valid_case(NULL, "%{aaai", 6, -2);
+    valid_case(NULL, "%}i", 3, -2);
+    valid_case(NULL, "%{aaa} ", 7, -2);
+    valid_case(NULL, "%{{aaa}i", 8, -2);
+    valid_case(NULL, "%{%aaa}i", 8, -2);
+    valid_case(NULL, NULL, 1, -1);
+    valid_case(NULL, "", 1, -1);
+    valid_case(NULL, "a", 0, -1);
+    valid_case(NULL, NULL, 0, -1);
+    valid_case(&trash_ptr, "%b", 2, -1);
+)
 
-    valid_case("%b",  2, 0);
-    valid_case("- -", 3, 0);
-    valid_case("%{aaa}i %{bb}i", 14, 0);
-    valid_case("%",  1, -2);
-    valid_case("% ", 2, -2);
-    valid_case("%{aaai", 6, -2);
-    valid_case("%}i", 3, -2);
-    valid_case("%{aaa} ", 7, -2);
-    valid_case("%{{aaa}i", 8, -2);
-    valid_case("%{%aaa}i", 8, -2);
-    valid_case(NULL, 1, -1);
-    valid_case("", 1, -1);
-    valid_case("a", 0, -1);
-    valid_case(NULL, 0, -1);
+CHEAT_TEST(create_list_head_ok,
+    const char *s = (const char *) 0x10;
+    struct formatter *ret = create_list_head(s, 33);
+    cheat_assert(ret != NULL);
+    cheat_assert(ret->first == NULL);
+    cheat_assert(ret->last == NULL);
+    cheat_assert(ret->format == s);
+    cheat_assert_int32(ret->format_len, 33);
+    free(ret);
+)
+
+CHEAT_TEST(list_add_on_empty_elements,
+    struct formatter dummy = { NULL, 0, NULL, NULL };
+    list_add_at_end(&dummy, 5, 1, -1);
+    cheat_assert(dummy.first != NULL);
+    cheat_assert(dummy.last != NULL);
+    cheat_assert(dummy.last == dummy.first);
+
+    cheat_assert_int32(dummy.first->position_start, 5);
+    cheat_assert_int32(dummy.first->position_end, 1);
+    cheat_assert_int32(dummy.first->registered_index, -1);
+    cheat_assert(dummy.first->next == NULL);
+    free(dummy.first);
+)
+
+CHEAT_TEST(list_add_on_one_or_more_elements,
+    struct chain_formatter first = { 5, 1, -1, NULL };
+    struct formatter dummy = { NULL, 0, &first, &first };
+    list_add_at_end(&dummy, 7, 7, 3);
+    cheat_assert(dummy.first == &first);
+    cheat_assert(dummy.last != NULL);
+    cheat_assert(dummy.last != dummy.first);
+
+    cheat_assert_int32(dummy.last->position_start, 7);
+    cheat_assert_int32(dummy.last->position_end, 7);
+    cheat_assert_int32(dummy.last->registered_index, 3);
+    cheat_assert(dummy.last->next == NULL);
+    cheat_assert(dummy.first->next == dummy.last);
 )
