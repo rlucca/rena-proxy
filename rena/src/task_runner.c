fb4c217589f2d56c7ec2fb24921a8c6b9c96d228
Ricardo Rodrigues Lucca <ricardo.lucca@rnp.br>
2022-07-05 16:19:07 -0300

    task_runner: limpando a chamada do server_dispatch

--- rena/src/task_runner.c
+++ rena/src/task_runner.c
@@ -10,21 +10,8 @@
 #include <sys/epoll.h>
 #include <pthread.h>
 
-static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-
 static void task_handling(struct rena *rena, task_t *task);
 
-
-static int has_io(int io)
-{
-    static int ret = -1;
-    if (io != 0)
-    {
-        ret = io;
-    }
-
-    return ret;
-}
 
 void *task_runner(void *arg)
 {
@@ -32,38 +19,12 @@ void *task_runner(void *arg)
 
     while (rena->forced_exit == 0)
     {
-        int do_dispatch = 0;
-        if (has_io(0) < 0)
-        {
-            THREAD_CRITICAL_BEGIN(lock)
-            if (has_io(0) < 0)
-            {
-                has_io(1);
-                do_dispatch = 1;
-            }
-            THREAD_CRITICAL_END(lock)
-        }
-
-        if (do_dispatch != 0)
-        {
-            task_manager_set_working(rena, 1);
-            server_dispatch(rena);
-            task_manager_set_working(rena, 0);
-
-            THREAD_CRITICAL_BEGIN(lock)
-            if (has_io(0) != 0)
-            {
-                has_io(-1);
-            }
-            THREAD_CRITICAL_END(lock)
-        } else {
-            task_t *task = NULL;
-            task_manager_set_working(rena, 1);
-            task = task_manager_task_consume(rena);
-            task_handling(rena, task);
-            task_manager_set_working(rena, 0);
-            task_manager_task_free(&task);
-        }
+        task_t *task = NULL;
+        task_manager_set_working(rena, 1);
+        task = task_manager_task_consume(rena);
+        task_handling(rena, task);
+        task_manager_set_working(rena, 0);
+        task_manager_task_free(&task);
     }
 
     task_manager_forced_exit(rena);
